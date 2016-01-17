ec2c575457cdf8e77e8e2b5bfa37d1f1a5b58178
mira49 <miratouatii@gmail.com>
2016-01-17 02:04:45 +0100

    groups

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ MongoClient.connect(url, function(err, db) {
 	db.collection("users", function(err, users) {
 
 		app.post('/users/register', function(req, res) {
-			console.log(req.body.pseudo+" "+req.body.email+" "+req.body.password)
+
 
 			if(req.body.password.length < 6) return res.json({error:"Mot de passe trop court"}).end()
 				if(req.body.pseudo.length < 6) return res.json({error:"Pseudo trop court"}).end()	
@@ -60,6 +60,7 @@ MongoClient.connect(url, function(err, db) {
 		app.post('/users/friends', function(req, res) {
 
 			if (req.body.email != req.session.user.email) {
+if(req.body.pseudo.length < 6) return res.json({error:"Pseudo trop court"}).end()
 				users.findOne({email:req.body.email}, function(err, user){
 
 					if(err) return;
@@ -78,6 +79,7 @@ MongoClient.connect(url, function(err, db) {
 								pseudo : req.body.pseudo
 							}
 							db.collection("friends", function(err, friends) {
+
 								friends.findOne( { 
 									$or : [ 
 									{$and : [ { "user.email" : CurrentUser.email }, { "friend.email": Friend.email} ]},
@@ -112,15 +114,10 @@ MongoClient.connect(url, function(err, db) {
 
 		app.get('/users/friends', function(req, res) {
 
-			var CurrentUser = {
-				email : req.session.user.email,
-				pseudo : req.session.user.pseudo
-			}
-
-			console.log(CurrentUser)
-
+			
 			db.collection("friends", function(err, friends) {
-				var cursor = friends.find({$or : [ { user : CurrentUser }, { friend : CurrentUser } ]})
+
+				var cursor = friends.find({$or : [ { "user.email" : req.session.user.email }, { "friend.email" : req.session.user.email } ]})
 				cursor.toArray(function(err, data) {
 					if (err) return next(err)
 						console.log(data)
@@ -128,13 +125,14 @@ MongoClient.connect(url, function(err, db) {
 					var length = Object.keys(data).length; 
 					for (var i = 0; i <length; i++) 
 					{
-						if(data[i].friend == CurrentUser)
+
+						if(data[i].friend.email == req.session.user.email)
 							tab[i] = data[i].user;  
 						else tab[i] = data[i].friend;  
 
 					}	
 
-					console.log(tab)
+					
 					res.jsonp(tab)
 				})
 			})
@@ -146,30 +144,54 @@ MongoClient.connect(url, function(err, db) {
 
 
 		app.get('/users/groups', function(req, res) {
-			var cursor = groups.find({membres: {$elemMatch: {email:req.session.user.email}}})
+
+//groupes non partagé
+			var cursor = groups.find({"membres.0.email": req.session.user.email})
+//dans le cas du pseudo juste
+//			var cursor = groups.find({membres: { $all:[req.session.user.pseudo]}})
+//dans le cas des groupes partagé
+//			var cursor = groups.find({membres: {$elemMatch: {email:req.session.user.email}}})
 			cursor.toArray(function(err, data) {
 				if (err) return next(err)
-					var tab=new Array()
+
+				var tab=new Array()
 				var length = Object.keys(data).length; 
 				for (var i = 0; i <length; i++) 
 				{	
+
 					tab[i]= {}
 					tab[i].nameGroupe= data[i].nameGroupe;
 					tab[i].membres=new Array();
+				var k=0;
+				var lengthMembres = Object.keys(data[i].membres).length;
+			for (var j = 0; j <lengthMembres; j++) 
+				{
 
-					if(data[i].membres.email != req.session.user.email)
-						tab[i].membres = data[i].membres.pseudo;    
+					if(data[i].membres[j].pseudo != req.session.user.pseudo)
+						{
+						tab[i].membres[k] = { pseudo: data[i].membres[j].pseudo,
+						email: data[i].membres[j].email
+							}
+						k++;
+						 
+						}  
+}
 
-				}	
+	
+				}
+	
 				res.jsonp(tab)
 			})
 
 		});
 
 		app.post('/users/groups', function(req, res) {
-
-
-			groups.insert({nameGroupe:req.body.nameGroupe, membres:req.body.groups}, function(err, groups){
+var lengthMembres = Object.keys(req.body.membres).length;
+			for (var j = 0; j <lengthMembres; j++) 
+				{
+if(req.body.membres[j].pseudo.length < 6) return res.json({error:"Pseudo trop court"}).end()
+}
+			groups.insert({nameGroupe:req.body.nameGroupe, membres:req.body.membres}, function(err, groups){
 				console.log("Insertion groupe reussie")
 				res.json(groups.ops[0]).end()
 			})
