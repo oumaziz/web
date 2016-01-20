9b7706efbc6904db83d68c9d927ebd690ece95bb
mira49 <miratouatii@gmail.com>
2016-01-20 12:09:01 +0100

    modification groupe terminé

--- public/js/app.js
+++ public/js/app.js
@@ -196,20 +196,52 @@ app.controller("DashboardController", ['$scope', '$resource', '$rootScope', '$co
         var addMember= $scope.MemberNumber.length+1;
 
         $scope.GroupView = function(group) {
-            $scope.groupsUpdate.CurrentGroup=group;
-            $scope.MemberNumberGroupUpdate=[];
-            for(var k=0; k< $scope.groupsUpdate.CurrentGroup.membres.length-1 ; k++)
-             $scope.MemberNumberGroupUpdate[k]=k+1;
-
-     }
-
-     $scope.changePseudo = function(number) {
-
-        for(var j=0; j <$scope.Listefriends.length; j++ ) {
-            if($scope.groups.membres[number].pseudo==$scope.Listefriends[j].pseudo) 
-                $scope.groups.membres[number].email=$scope.Listefriends[j].email;
+            
+        $scope.groupsUpdate.CurrentGroup=group;
+        $scope.MemberNumberGroupUpdate=[];
+        if($scope.groupsUpdate.CurrentGroup.membres[0].email == $rootScope.currentUser.email ){
+            $scope.boole=true;
+        for(var k=0; k< $scope.groupsUpdate.CurrentGroup.membres.length-1 ; k++)
+                { 
+              $scope.MemberNumberGroupUpdate[k]=k+1;
+              nbMember=k+1;
+              nombre=$scope.groupsUpdate.CurrentGroup.membres.length
+                }
+            }
+            else {  $scope.boole=false;
+             Notification.error({message: "Vous ne pouvez pas modifier le groupe", positionY: 'bottom', positionX: 'right'});}
         }
-    }
+         
+         $scope.addGroup = function() {
+            nbMember= nbMember+1
+            $scope.MemberNumberGroupUpdate.push(nbMember);
+        }
+
+        $scope.removeGroup = function(number) {
+            if(nombre> 3){
+            var index2 = this.MemberNumberGroupUpdate.indexOf(number);
+            this.MemberNumberGroupUpdate.splice(index2, 1);
+            nombre--;
+        }
+        else 
+            Notification.error({message: "Il faut au moins 2 membres", positionY: 'bottom', positionX: 'right'});
+        };
+
+        $scope.changePseudo = function(number) {
+
+            for(var j=0; j <$scope.Listefriends.length; j++ ) {
+                if($scope.groups.membres[number].pseudo==$scope.Listefriends[j].pseudo) 
+                    $scope.groups.membres[number].email=$scope.Listefriends[j].email;
+            }
+        }
+
+        $scope.changePseudoUpdateGroup = function(number) {
+
+            for(var j=0; j <$scope.Listefriends.length; j++ ) {
+                if($scope.groupsUpdate.CurrentGroup.membres[number].pseudo==$scope.Listefriends[j].pseudo) 
+                    $scope.groupsUpdate.CurrentGroup.membres[number].email=$scope.Listefriends[j].email;
+            }
+        }
 
     $scope.remove = function(number) {
         var index = this.MemberNumber.indexOf(number);
@@ -253,6 +285,65 @@ app.controller("DashboardController", ['$scope', '$resource', '$rootScope', '$co
         Notification.error({message: "Une erreur s'est produite", positionY: 'bottom', positionX: 'right'});
     });
 }
+
+}]);
+app.controller("FriendsController", ['$scope', '$resource', '$rootScope', '$cookies', '$location', 'Notification',
+ function ($scope, $resource, $rootScope, $cookies, $location, Notification){
+
+     var Friends = $resource("http://localhost:3000/users/friends");
+
+     $scope.friends = new Friends();
+
+     var FriendsUpdate = $resource("http://localhost:3000/users/friends/update");
+
+     $scope.friendsUpdate = new FriendsUpdate();
+
+     Friends.query(function(result){
+
+         $rootScope.Listefriends = result;
+         $rootScope.initFromFriends()
+     })
+     $scope.FriendView = function(friend) {
+        $scope.friendsUpdate.CurrentFriend=friend;
+
+    }
+
+    $scope.envoyer = function() {
+
+     $scope.friends.$save(function(result){
+
+         if(result.error == null){
+
+             location.reload(); 
+
+         }else{
+             Notification.error({message: result.error, positionY: 'bottom', positionX: 'right'});
+         }
+     }).catch(function(req){
+         Notification.error({message: "Une erreur s'est produite", positionY: 'bottom', positionX: 'right'});
+     });
+ }
+
+
+ $scope.update = function() {
+    $scope.friendsUpdate.$save(function(result){
+
+        if(result.error == null){
+
+         location.reload(); 
+
+     }else{
+         Notification.error({message: result.error, positionY: 'bottom', positionX: 'right'});
+     }
+ }).catch(function(req){
+     Notification.error({message: "Une erreur s'est produite", positionY: 'bottom', positionX: 'right'});
+ });
+}
+
+
+
+
+
 
 }]);
 
