ad6667088d1d19019b9e458c38fbef75a0cce047
mira49 <miratouatii@gmail.com>
2016-01-20 21:17:50 +0100

    integration

--- public/js/controllers/GroupsController.js
+++ public/js/controllers/GroupsController.js
@@ -20,76 +20,146 @@ angular.module("expensesApp").controller("GroupsController", ['$scope', '$resour
     
     $rootScope.refreshGroups()
 
-    $scope.MemberNumber = [1,2];
-    $scope.groups.membres = [];
-    $scope.groups.membres[0] = {
-      pseudo:$rootScope.currentUser.pseudo,
-      email:$rootScope.currentUser.email
+     $scope.groups.membres = [];
+        $scope.groups.membres[0] = {
+            pseudo:$rootScope.currentUser.pseudo,
+            email:$rootScope.currentUser.email
 
-    }
-    var addMember= $scope.MemberNumber.length+1;
+        }
+
+         $scope.groups.membres[1] = {
+            pseudo:null,
+            email:null
+
+        }
+
+         $scope.groups.membres[2] = {
+            pseudo:null,
+            email:null
+
+        }
 
     $scope.GroupView = function(group) {
-
-      $scope.groupsUpdate.CurrentGroup=group;
-      $scope.MemberNumberGroupUpdate=[];
-      if($scope.groupsUpdate.CurrentGroup.membres[0].email == $rootScope.currentUser.email ){
-        $scope.boole=true;
-        for(var k=0; k< $scope.groupsUpdate.CurrentGroup.membres.length-1 ; k++)
-        { 
-          $scope.MemberNumberGroupUpdate[k]=k+1;
-          nbMember=k+1;
-          nombre=$scope.groupsUpdate.CurrentGroup.membres.length
-          if(nombre>3) $scope.varShow=true; else
-          $scope.varShow=false;
-        }
-      }
-      else {  $scope.boole=false;
-       Notification.error({message: "Vous ne pouvez pas modifier le groupe", positionY: 'bottom', positionX: 'right'});}
+$scope.groupsUpdate.CurrentGroup=group;
+        if($scope.groupsUpdate.CurrentGroup.membres[0].email == $rootScope.currentUser.email ){
+            $scope.boole=true;
+            nombre=$scope.groupsUpdate.CurrentGroup.membres.length
+              
+              if(nombre>3) $scope.varShow=true; else
+                 $scope.varShow=false;
+                
+            }
+            else {  $scope.boole=false;
+             Notification.error({message: "Vous ne pouvez pas modifier le groupe", positionY: 'bottom', positionX: 'right'});}
+        
      }
 
      $scope.addGroup = function() {
-      nbMember= nbMember+1
-      $scope.MemberNumberGroupUpdate.push(nbMember);
+     $scope.groupsUpdate.CurrentGroup.membres.push({});
+            nombre++;
+            if(nombre>3) $scope.varShow=true; else
+            $scope.varShow=false;
     }
 
-    $scope.removeGroup = function(number) {
+    $scope.removeGroup = function(group) {
       if(nombre> 3){
-        var index2 = this.MemberNumberGroupUpdate.indexOf(number);
-        this.MemberNumberGroupUpdate.splice(index2, 1);
-        nombre--;
-        if(nombre>3) $scope.varShow=true; else
-        $scope.varShow=false;
-      }
-
+            var index2 = $scope.groupsUpdate.CurrentGroup.membres.indexOf(group);
+            
+            $scope.groupsUpdate.CurrentGroup.membres.splice(index2,1)
+            nombre--;
+            if(nombre>3) $scope.varShow=true; else
+            $scope.varShow=false;
+        } else {
+            Notification.error({message: "il faut au moin 2 membres", positionY: 'bottom', positionX: 'right'});
+        }
 
     };
 
-    $scope.changePseudo = function(number) {
+    $scope.changePseudo = function(membre) {
+        var temp=true;
+            var temp2=true;
+            for(var i=0; i < $scope.groups.membres.length; i++ ) {
+                        if($scope.groups.membres[i]!=membre){
+                             if($scope.groups.membres[i].pseudo == membre.pseudo){
+                                temp=false
+                                membre.pseudo=null;
 
-      for(var j=0; j <$scope.Listefriends.length; j++ ) {
-        if($scope.groups.membres[number].pseudo==$scope.Listefriends[j].pseudo) 
-          $scope.groups.membres[number].email=$scope.Listefriends[j].email;
-      }
+                                 Notification.error({message: "Ce pseudo existe deja", positionY: 'bottom', positionX: 'right'});
+                             }
+
+                     }
+                 }
+
+
+                if(temp==true) {
+                                
+                 for(var j=0; j <$scope.Listefriends.length; j++ ) {
+                                    if(membre.pseudo==$scope.Listefriends[j].pseudo) 
+                                         {membre.email=$scope.Listefriends[j].email;
+                                            temp2=false
+                                         }
+                                       
+                                }
+                }
+
+                if(temp2==true)  membre.email=null
     }
 
-    $scope.changePseudoUpdateGroup = function(number) {
 
-      for(var j=0; j <$scope.Listefriends.length; j++ ) {
-        if($scope.groupsUpdate.CurrentGroup.membres[number].pseudo==$scope.Listefriends[j].pseudo) 
-          $scope.groupsUpdate.CurrentGroup.membres[number].email=$scope.Listefriends[j].email;
-      }
+    
+     $scope.changeEmail = function(membre) {
+           
+           for(var i=0; i < $scope.groups.membres.length; i++ ) {
+                        if($scope.groups.membres[i]!=membre && membre.email!= null){
+                        
+                         if($scope.groups.membres[i].email ==membre.email) membre.email=null;
+                     }
+                         }
+            
+        }
+        
+    $scope.changePseudoUpdateGroup = function(member) {
+
+     
+             var temp=true;
+            var temp2=true;
+            for(var i=0; i < $scope.groupsUpdate.CurrentGroup.membres.length; i++ ) {
+                        if($scope.groupsUpdate.CurrentGroup.membres[i]!=member){
+                             if($scope.groupsUpdate.CurrentGroup.membres[i].pseudo == member.pseudo){
+                                temp=false
+                                 Notification.error({message: "Ce pseudo existe deja", positionY: 'bottom', positionX: 'right'});
+                                 member.pseudo=null;
+                             }
+
+                     }
+                 }
+
+
+                if(temp==true) {
+                                
+                 for(var j=0; j <$scope.Listefriends.length; j++ ) {
+                                    if(member.pseudo==$scope.Listefriends[j].pseudo) 
+                                         {member.email=$scope.Listefriends[j].email;
+                                            temp2=false
+                                         }
+                                       
+                                }
+                }
+
+                if(temp2==true)  member.email=null
+
+            
     }
 
-    $scope.remove = function(number) {
-      var index = this.MemberNumber.indexOf(number);
-      this.MemberNumber.splice(index, 1);
+    $scope.remove = function(membre) {
+      var index3 = $scope.groups.membres.indexOf(membre);
+        $scope.groups.membres.splice(index3,1)
     };
 
 
 
     $scope.add = function() {
-      $scope.MemberNumber.push(addMember++);
+     $scope.groups.membres.push({});
     }
 
     $scope.updateGroup = function() {
