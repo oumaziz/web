d5ec38521d1f835553bd8758fcf6093b619336c8
Mohamed Oumaziz <mohamed@oumaziz.com>
2016-01-21 11:16:24 +0100

    Ajout BDD

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,10 @@
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { 
-	if(!error) console.log("\nSUCCESS : " + stdout)
-	else console.log("\nERROR :\n" + error)
+	if(error) console.log("\nERROR :\n" + error)
 }
 
 // Liste des commandes
 exec("bower install", puts)
-exec("mongoimport --db oumaziz_web --collection demo demo.json", puts)
-
-// mongoexport --db oumaziz_web --collection groups --out groups.json
-// mongoimport --db test --collection test_collection_groups --file groups.json
+exec("mongoimport --db oumaziz_web --collection users scripts/database/users.json", puts)
+exec("mongoimport --db oumaziz_web --collection friends scripts/database/friends.json", puts)
+exec("mongoimport --db oumaziz_web --collection groups scripts/database/groups.json", puts)
